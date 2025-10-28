26a28c2e8c5c755210ee0d9fb0ed142479cd2561
NikoleRubiano <rubianonicole6@gmail.com>
2025-10-27 21:35:17 -0500

    "asincronizmo"

--- function.js
+++ function.js
@@ -131,3 +131,115 @@ const wordfrecuency = words.reduce((acumulador, currentvalue) => {
     return acumulador
 }, {})
 console.log(wordfrecuency);
+
+
+/* 
+Requerimientos del reto:
+
+1. El usuario debe poder ingresar su usuario y contraseÃ±a
+2. El sistema debe ser capaz de validar si el usuario y contraseÃ±a ingresados por el usuario existen en la base de datos
+3. Si el usuario y contraseÃ±a son correctos, el sistema debe mostrar un mensaje de bienvenida y mostrar el timeline del usuario.
+4. Si el usuario y contraseÃ±a son incorrectos, el sistema debe mostrar un mensaje de error y no mostrar ningun timeline.
+
+*/
+
+/*
+const usersDatabase = [
+  {
+    username: "andres",
+    password: "123",
+  },
+  {
+    username: "caro",
+    password: "456",
+  },
+  {
+    username: "mariana",
+    password: "789",
+  },
+];
+
+const usersTimeline = [
+  {
+    username: "Estefany",
+    timeline: "Me encata Javascript!",
+  },
+  {
+    username: "Oscar",
+    timeline: "Bebeloper es lo mejor!",
+  },
+  {
+    username: "Mariana",
+    timeline: "A mi me gusta mÃ¡s el cafÃ© que el tÃ©",
+  },
+  {
+    username: "Andres",
+    timeline: "Yo hoy no quiero trabajar",
+  },
+];
+
+const username = prompt("Cual es tu usuario?");
+const password = prompt("Cual es tu contraseña?");
+
+function usuarioExistente(username, password) {
+  for (let i = 0; i < usersDatabase.length; i++) {
+    if (
+      usersDatabase[i].username === username &&
+      usersDatabase[i].password === password
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function signIn(username, password) {
+  if (usuarioExistente(username, password)) {
+    alert(`Bienvenido a tu cuenta ${username}`);
+    console.log(usersTimeline);
+  } else {
+    alert("Uuups, usuario o contraseña incorrectos!");
+  }
+}
+
+signIn(username, password);  
+*/
+
+
+
+const foo = () => console.log("Firts");
+const bar = () => setTimeout(() => console.log("Second"), 0);
+const baz = () => console.log("Third");
+
+foo();
+bar();
+baz();
+
+const promise = new Promise((resolve,
+     reject) => {
+        setTimeout(() => {
+            let operationSuccesful = true;
+            if (operationSuccesful) {
+                resolve('la opreracion fue exitosa');
+            } else {
+                reject('la operacion fallo');
+            }
+     }, 2000);
+});
+
+promise
+.then((succesMesasge) => {
+    console.log(succesMesasge);
+})
+.catch((errorMessage) => {
+    console.log(errorMessage);
+});
+
+function fetchData() {
+    fetch("https://rickandmortyapi.com/api/character")
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+}
+
+fetchData();
